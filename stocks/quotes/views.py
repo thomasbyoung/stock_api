81d086fe07a81e375ce679cfad192526f80e97ac
thomasbyoung <thomasbyoung12345@gmail.com>
2020-06-18 15:59:50 -0500

    form connected to api

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -4,14 +4,15 @@ def home(request):
     import requests
     import json
 
-
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e: 
-        api = "Error..."
-
-    return render(request, 'home.html', {'api': api})
-
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e: 
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': "Please enter a ticker."})
 
 def about(request):
     return render(request, 'about.html', {})
