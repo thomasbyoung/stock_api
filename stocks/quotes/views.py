fa3925f49565e39e6966a9cf08978bb5d934c77d
thomasbyoung <thomasbyoung12345@gmail.com>
2020-06-26 10:54:05 -0500

    configuring table

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -21,6 +21,9 @@ def about(request):
     return render(request, 'about.html', {})
 
 def add_stock(request):
+    import requests
+    import json
+    
     if request.method == 'POST':
         form = StockForm(request.POST or None)
 
@@ -29,9 +32,16 @@ def add_stock(request):
             messages.success(request, ("Stock has been added"))
             return redirect('add_stock')
     else:
-    
         ticker = Stock.objects.all()
-        return render(request, 'add_stock.html', {'ticker': ticker})
+        output = []
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/batch?types=quote,news,chart&range=1m&last=10&token=sk_b54bf28c97aa43e6ba0179653ba6fbac")
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e: 
+                api = "Error..."
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
